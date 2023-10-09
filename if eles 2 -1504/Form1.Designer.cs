982017d34d98526bc7ab2ea5518b35dd585b276f
akinseman <145451296+akinseman@users.noreply.github.com>
2023-10-09 15:36:43 +0300

    Add files via upload

---
+++ if eles 2 -1504/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace if_eles_2__1504
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rbRenk1 = new System.Windows.Forms.RadioButton();
+            this.rbRenk2 = new System.Windows.Forms.RadioButton();
+            this.rbRemk3 = new System.Windows.Forms.RadioButton();
+            this.rbrenki4 = new System.Windows.Forms.RadioButton();
+            this.btndDeşiştiir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rbRenk1
+            // 
+            this.rbRenk1.AutoSize = true;
+            this.rbRenk1.Location = new System.Drawing.Point(13, 23);
+            this.rbRenk1.Name = "rbRenk1";
+            this.rbRenk1.Size = new System.Drawing.Size(58, 17);
+            this.rbRenk1.TabIndex = 0;
+            this.rbRenk1.TabStop = true;
+            this.rbRenk1.Text = "Pempe";
+            this.rbRenk1.UseVisualStyleBackColor = true;
+            // 
+            // rbRenk2
+            // 
+            this.rbRenk2.AutoSize = true;
+            this.rbRenk2.Location = new System.Drawing.Point(13, 55);
+            this.rbRenk2.Name = "rbRenk2";
+            this.rbRenk2.Size = new System.Drawing.Size(43, 17);
+            this.rbRenk2.TabIndex = 1;
+            this.rbRenk2.TabStop = true;
+            this.rbRenk2.Text = "Sarı";
+            this.rbRenk2.UseVisualStyleBackColor = true;
+            // 
+            // rbRemk3
+            // 
+            this.rbRemk3.AutoSize = true;
+            this.rbRemk3.Location = new System.Drawing.Point(13, 79);
+            this.rbRemk3.Name = "rbRemk3";
+            this.rbRemk3.Size = new System.Drawing.Size(54, 17);
+            this.rbRemk3.TabIndex = 2;
+            this.rbRemk3.TabStop = true;
+            this.rbRemk3.Text = "Kırmızı";
+            this.rbRemk3.UseVisualStyleBackColor = true;
+            // 
+            // rbrenki4
+            // 
+            this.rbrenki4.AutoSize = true;
+            this.rbrenki4.Location = new System.Drawing.Point(12, 102);
+            this.rbrenki4.Name = "rbrenki4";
+            this.rbrenki4.Size = new System.Drawing.Size(71, 17);
+            this.rbrenki4.TabIndex = 3;
+            this.rbrenki4.TabStop = true;
+            this.rbrenki4.Text = "Turunucu";
+            this.rbrenki4.UseVisualStyleBackColor = true;
+            // 
+            // btndDeşiştiir
+            // 
+            this.btndDeşiştiir.Location = new System.Drawing.Point(13, 125);
+            this.btndDeşiştiir.Name = "btndDeşiştiir";
+            this.btndDeşiştiir.Size = new System.Drawing.Size(104, 44);
+            this.btndDeşiştiir.TabIndex = 4;
+            this.btndDeşiştiir.Text = "button1";
+            this.btndDeşiştiir.UseVisualStyleBackColor = true;
+            this.btndDeşiştiir.Click += new System.EventHandler(this.btndDeşiştiir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(128, 175);
+            this.Controls.Add(this.btndDeşiştiir);
+            this.Controls.Add(this.rbrenki4);
+            this.Controls.Add(this.rbRemk3);
+            this.Controls.Add(this.rbRenk2);
+            this.Controls.Add(this.rbRenk1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton rbRenk1;
+        private System.Windows.Forms.RadioButton rbRenk2;
+        private System.Windows.Forms.RadioButton rbRemk3;
+        private System.Windows.Forms.RadioButton rbrenki4;
+        private System.Windows.Forms.Button btndDeşiştiir;
+    }
+}
+
---
+++ if eles 2 -1504/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace if_eles_2__1504
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cbSari_CheckedChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void btndDeşiştiir_Click(object sender, EventArgs e)
+        {
+            if (rbRenk1.Checked)
+            {
+                this.BackColor = Color.Pink;
+
+            }
+            else if (rbRenk2.Checked)
+            {
+                this.BackColor =Color.Yellow;
+            }
+            else if (rbRemk3.Checked)
+            {
+                this.BackColor=Color.Red;
+            }
+            else if (rbrenki4.Checked)
+            {
+                this.BackColor = Color.DarkOrange;
+            }
+       } 
+    }
+}
